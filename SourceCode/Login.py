4467fe25c412fa54c69cac1583b3e680d0be15fc
Quirk <vx7166wj@winona.edu>
2019-04-23 14:06:35 -0500

    Uploading basic login windows (files)

---
+++ SourceCode/Login.py
@@ -0,0 +1,55 @@
+import wx
+
+class Login(wx.Frame):
+    def login_clicked(self, e):
+        self.Hide()
+        print("Button Pressed.")
+        from Note_Window import NoteWindow
+
+    def exit_clicked(event, e):
+        print("Time to go!")
+        quit()
+
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title)
+        self.panel = wx.Panel(self)
+        self.loginfield = wx.TextCtrl(self.panel, size=(140, -1))
+        self.logintitle = wx.StaticText(self.panel, label="Enter your username")
+        self.passfield = wx.TextCtrl(self.panel, size=(140, -1), style=wx.TE_PASSWORD)
+        self.passtitle = wx.StaticText(self.panel, label="Enter your password")
+
+        self.loginbutton = wx.Button(self.panel, label="Login", pos=(130, 10), size=(60,30))
+        self.exitbutton = wx.Button(self.panel, label="Exit", pos=(130, 50), size=(60,30))
+        self.loginbutton.Bind(wx.EVT_BUTTON, self.login_clicked)
+        self.exitbutton.Bind(wx.EVT_BUTTON, self.exit_clicked)
+
+        # Set sizer for the frame, so we can change frame size to match widgets
+        self.windowSizer = wx.BoxSizer()
+        self.windowSizer.Add(self.panel, 1, wx.ALL | wx.EXPAND)
+
+        # Set sizer for the panel content
+        self.sizer = wx.GridBagSizer(5, 5)
+        self.sizer.Add(self.logintitle, (0, 0))
+        self.sizer.Add(self.loginfield, (1, 0))
+        self.sizer.Add(self.passtitle, (2, 0))
+        self.sizer.Add(self.passfield, (3, 0))
+        self.sizer.Add(self.loginbutton, (4, 0))
+        self.sizer.Add(self.exitbutton, (5, 0))
+
+        # Set simple sizer for a nice border
+        self.border = wx.BoxSizer()
+        self.border.Add(self.sizer, 1, wx.ALL | wx.EXPAND, 5)
+
+        # Use the sizers
+        self.panel.SetSizerAndFit(self.border)
+        self.SetSizerAndFit(self.windowSizer)
+
+app = wx.App(False)
+frame = Login(None, "Notekeeper!")
+frame.Center()
+frame.Show()
+app.MainLoop()
+
+
+#Code partially taken from https://stackoverflow.com/questions/14927584/simple-example-of-using-wx-textctrl-and-display-data-after-button-click-in-wxpyt
+#by contributor Fenikso on 2/18/2013
---
+++ SourceCode/Note_Window.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+import wx
+
+def OnFrameExit(event):
+    quit()
+
+class NoteWindow(wx.Frame):
+    def __init__(self, parent, title):
+        wx.Frame.__init__(self, parent, title=title, size=(200,100))
+        self.control = wx.TextCtrl(self, style=wx.TE_MULTILINE)
+        self.CreateStatusBar()
+
+        filemenu= wx.Menu()
+
+        filemenu.Append(wx.ID_NEW, "New"," Start a new Note")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_SAVE, "Save"," Save your note")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_SAVEAS, "Save as"," Save your note to a specific directory")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_ABOUT, "&About"," Information about this program")
+        filemenu.AppendSeparator()
+        filemenu.Append(wx.ID_EXIT, "E&xit"," Terminate the program")
+
+        menuBar = wx.MenuBar()
+        menuBar.Append(filemenu,"&File")
+        self.SetMenuBar(menuBar)
+        self.Show(True)
+
+        #Adds a button event listener to exit the program.
+        self.Bind(wx.EVT_MENU, OnFrameExit, id=wx.ID_EXIT)
+
+app = wx.App(False)
+frame = NoteWindow(None, "Sample editor")
+app.MainLoop()
